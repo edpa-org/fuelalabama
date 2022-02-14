117a61436265bde710780ab082dfa322af8a687a
jflayhart <jflayhart@shipt.com>
2022-02-13 20:11:00 -0600

    Add featured image to site

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,5 @@
 <script context="module">
 	import Apply from '$lib/Apply.svelte'
-	import WhyAlabama from '$lib/WhyAlabama.svelte'
 	import About from '$lib/About.svelte'
 	import Internships from '$lib/Internships.svelte'
 	import Footer from '$lib/Footer.svelte'
@@ -8,7 +7,22 @@
 	import Play from '$lib/Play.svelte'
 	import Live from '$lib/Live.svelte'
 	import GreenTitle from '$lib/GreenTitle.svelte'
+</script>
 
+<script>
+    import {onMount} from 'svelte'
+    const apiUrl = import.meta.env.VITE_API_URL
+    let heroPage = {};
+    onMount(async () => {
+        const res = await fetch(`${apiUrl}/wp/v2/pages?slug=hero-image`)
+        heroPage = await res.json()
+        heroPage = heroPage[0]
+        //gets the featured image
+        const imageApi = heroPage._links["wp:featuredmedia"][0].href;
+        const imgRes = await fetch(imageApi)
+        const image = await imgRes.json()
+        heroPage.image = image.source_url
+    })
 </script>
 
 <svelte:head>
@@ -27,7 +41,9 @@
 
 </style>
 
-<div id='skyline'><img alt='alabama' src="/city.jpeg"/></div>
+<div id='skyline'>
+    <img alt='alabama' src={heroPage.image}/>
+</div>
 <GreenTitle/>
 <div id="home">
   <Work/>
