7f897d1868e4a9370d7a49d838497484f3f33bb1
Quynh <quynh.do@shipt.com>
2022-02-21 13:09:41 -1000

    move host a fellow to apply section

--- src/lib/Apply.svelte
+++ src/lib/Apply.svelte
@@ -18,6 +18,22 @@
     <p class="section-body">{@html post.content.rendered}</p>
   </div>
 {/if}
+<div class="section-container-employers">
+  <div class="horizontal-line"></div>
+  <div class="section-title-employers-small">
+    Employers
+  </div>
+  <div class="section-title-wrapper-employers">
+    <div class="vertical-line">
+    </div>
+    <div class="section-title-employers">
+      Host a Fellow
+    </div>
+  </div>
+  <div class="button-wrapper">
+    <a class='button' rel="external" href="https://www.edpa.org">LEARN MORE AT EDPA.ORG</a>
+  </div>
+</div>
   
 <style lang="scss">
   .section-container {
@@ -53,6 +69,64 @@
     margin-bottom: 0;
     margin-left: 6%;
     text-align: left;
+	}
+
+  .section-container-employers {
+	margin-top: 50px;
+    position: relative;
+	display: flex;
+	flex-direction: column;
+	height: 300px;
+	margin-left: 50px;
+  }
+
+  .section-title-wrapper-employers {
+		display: flex;
+	}
+
+  .section-title-employers {
+    font-family: 'Gilmer-Bold';
+    font-size: 92px;
+    line-height: 92px;
+    color:white;
+	margin-left: 30px;
+    -webkit-text-stroke: 2px #1E1C5E;
+  }
+  
+  .section-title-employers-small {
+    font-family: 'Gilmer-Heavy';
+    font-size: 50px;
+    color:#1E1C5E;
+  }
+
+  .button-wrapper {
+	  display: flex;
+	  justify-content: center;
+	  margin-top: 30px;
+  }
+  a.button {
+	display:inline-block;
+	padding:0.46em 1.6em;
+	border:0.1em solid #000000;
+	border-radius: 5rem;
+	text-decoration:none;
+	font-family:'Gilmer-Light';
+	color:white;
+	text-align:center;
+	background-color: #1E1C5E;
+  }
+
+	.vertical-line {
+		display: block;
+		border-left: 6px solid #1E1C5E;
+		height: 100px;
+	}
+
+	.horizontal-line {
+	position: relative;
+	margin-left: 36px;
+	height: 30px;
+	background-color: #1E1C5E;
 	}
  
 @font-face {
--- src/lib/Internships.svelte
+++ src/lib/Internships.svelte
@@ -16,39 +16,23 @@
 </script>
 
 <div>
-  {#if page}
-	<div id={page.id} class="section-container-internships">
-				<div class="section-container-image">
-					<img alt="internships" src={page.image}/>
-				</div>
-				<div>
-					<div class="section-title-wrapper-internships">
-						<div class="section-title-internships">
-							{page.title.rendered}
+  	{#if page}
+		<div id={page.id} class="section-container-internships">
+					<div class="section-container-image">
+						<img alt="internships" src={page.image}/>
+					</div>
+					<div>
+						<div class="section-title-wrapper-internships">
+							<div class="section-title-internships">
+								{page.title.rendered}
+							</div>
+						</div>
+						<div class="section-body-wrapper">
+							<p class="section-body-internships">{@html page.content.rendered}</p>
 						</div>
 					</div>
-					<div class="section-body-wrapper">
-						<p class="section-body-internships">{@html page.content.rendered}</p>
-					</div>
-				</div>
-      </div>
-			{/if}
-	 <div class="section-container-employers">
-		  <div class="horizontal-line"></div>
-		<div class="section-title-employers-small">
-			Employers
 		</div>
-		<div class="section-title-wrapper-employers">
-			<div class="vertical-line">
-			</div>
-			<div class="section-title-employers">
-				Host a Fellow
-			</div>
-		</div>
-		<div class="button-wrapper">
-			<a class='button' rel="external" href="https://www.edpa.org">LEARN MORE AT EDPA.ORG</a>
-		</div>
-	 </div>
+	  {/if}
 </div>
 
 <style lang="css">
@@ -111,63 +95,6 @@
 		text-decoration: none;
 	}
 
-  .section-container-employers {
-	margin-top: 50px;
-    position: relative;
-	display: flex;
-	flex-direction: column;
-	height: 300px;
-	margin-left: 50px;
-  }
-
-  .section-title-wrapper-employers {
-		display: flex;
-	}
-
-  .section-title-employers {
-    font-family: 'Gilmer-Bold';
-    font-size: 92px;
-    line-height: 92px;
-    color:white;
-	margin-left: 30px;
-    -webkit-text-stroke: 2px #1E1C5E;
-  }
-  
-  .section-title-employers-small {
-    font-family: 'Gilmer-Heavy';
-    font-size: 50px;
-    color:#1E1C5E;
-  }
-
-  .button-wrapper {
-	  display: flex;
-	  justify-content: center;
-	  margin-top: 30px;
-  }
-  a.button {
-	display:inline-block;
-	padding:0.46em 1.6em;
-	border:0.1em solid #000000;
-	border-radius: 5rem;
-	text-decoration:none;
-	font-family:'Gilmer-Light';
-	color:white;
-	text-align:center;
-	background-color: #1E1C5E;
-  }
-
-	.vertical-line {
-		display: block;
-		border-left: 6px solid #1E1C5E;
-		height: 100px;
-	}
-
-	.horizontal-line {
-	position: relative;
-	margin-left: 36px;
-	height: 30px;
-	background-color: #1E1C5E;
-	}
 
  
 @font-face {
