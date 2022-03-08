59ec974fc22b555a5572a1b65f69fed76cc8373e
Quynh <quynh.do@shipt.com>
2022-03-08 16:41:37 -0500

    add mobile breakpoints to nav

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -11,7 +11,7 @@
 	const handleClick = (id) => document?.getElementById(id)?.scrollIntoView(true)
 </script>
 
-<style>
+<style lang='scss'>
 	nav {
 		font-weight: 300;
 		background-color: #1E1C5E;
@@ -19,22 +19,36 @@
 		height: 118px;
 		display: flex;
 		align-items: center;
+		@media screen and (max-width: 425px) {
+			flex-direction: column;
+		}
 	}
 
-	.title {
+	.logo {
 		display: flex;
 		flex-basis: 70%;
 		margin-left: 50px;
 	}
 
-	.title > img{
+	img {
+		width: 100%;
+		height: auto;
 		max-width: 200px;
+		@media screen and (max-width: 425px) {
+			max-width: 100px;
+		}
+		@media screen and (min-width: 425px) {
+			min-width: 100px;
+		}
 	}
 
 	ul.menu {
 		display: flex;
 		flex-basis: 30%;
 		padding-right: 20px;
+		@media screen and (min-width: 425px) {
+			padding-left: 0px;
+		}
 	}
 
 	li.menu {
@@ -44,13 +58,16 @@
 		text-transform: uppercase;
 		color: white;
 		cursor: pointer;
+		@media screen and (max-width: 768px) {
+			font-size: 12px;
+		}
 	}
 
 
 </style>
 
 <nav>
-	<div class="title"><img alt='logo' src='/logo.png'/></div>
+	<div class="logo"><img alt='logo' src='/logo.png'/></div>
 	<ul class='menu'>
 		{#each pages as p}
 			<li class='menu' on:click={()=>handleClick(p.id)}>
